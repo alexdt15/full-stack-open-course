80992a1e7d1ca9bc1b2f7cd0a6af12bc99c1739c
alexdt15 <alexdt15@gmail.co>
2025-04-27 13:16:31 +0200

    last parts courseinfo

--- part1/courseinfo/src/App.jsx
+++ part1/courseinfo/src/App.jsx
@@ -14,9 +14,9 @@ const Content = ({ allContent }) => {
   console.log("THIS IS CONTENT", allContent);
   return (
     <div>
-      <Part part={allContent.part1} exercises={allContent.exercises1} />
-      <Part part={allContent.part2} exercises={allContent.exercises2} />
-      <Part part={allContent.part3} exercises={allContent.exercises3} />
+      {allContent.map((part, index) => (
+        <Part key={index} part={part.name} exercises={part.exercises} />
+      ))}
     </div>
   );
 };
@@ -30,31 +30,32 @@ const Part = ({ part, exercises }) => {
 };
 
 const App = () => {
-  const course = "Half Stack application development";
-  const part1 = "Fundamentals of React";
-  const exercises1 = 10;
-  const part2 = "Using props to pass data";
-  const exercises2 = 7;
-  const part3 = "State of a component";
-  const exercises3 = 14;
-
-  const content = {
-    part1,
-    exercises1,
-    part2,
-    exercises2,
-    part3,
-    exercises3,
+  const course = {
+    name: "Half Stack application development",
+    parts: [
+      {
+        name: "Fundamentals of React",
+        exercises: 10,
+      },
+      {
+        name: "Using props to pass data",
+        exercises: 7,
+      },
+      {
+        name: "State of a component",
+        exercises: 14,
+      },
+    ],
   };
 
   return (
     <div>
-      <Header course={course} />
-      <Content allContent={content} />
+      <Header course={course.name} />
+      <Content allContent={course.parts} />
       <Total
-        exercises1={exercises1}
-        exercises2={exercises2}
-        exercises3={exercises3}
+        exercises1={course.parts[0].exercises}
+        exercises2={course.parts[1].exercises}
+        exercises3={course.parts[2].exercises}
       />
     </div>
   );
